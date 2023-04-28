506babe415c7541265c45e644ee14075fed73a2c
selene-cho <selene.cho29@gmail.com>
2023-04-29 01:45:46 +0900

    fix: 약어 검색착 수정

--- src/pages/AbbreviationPage/Abbr.jsx
+++ src/pages/AbbreviationPage/Abbr.jsx
@@ -5,11 +5,16 @@ import styles from './Abbr.module.scss';
 
 export default function Abbr() {
   const [abbrSearch, setAbbrSearch] = useState(''); // 약어 추천 변수명 검색수
-  const placeholder = '줄이고 싶은 변수명을 입력해주세요.';
+  const label = '줄이고 싶은 변수명을 입력해주세요.';
+  const placeholder = "'영어 단어'  ex) variable";
   return (
     <div className={styles.container}>
-      <AbbrResult abbrSearch={abbrSearch}></AbbrResult>
-      <SearchForm setSearch={setAbbrSearch} placeholder={placeholder}>
+      <AbbrResult abbrSearch={abbrSearch} />
+      <SearchForm
+        setSearch={setAbbrSearch}
+        placeholder={placeholder}
+        label={label}
+      >
         abbrSearch
       </SearchForm>
     </div>

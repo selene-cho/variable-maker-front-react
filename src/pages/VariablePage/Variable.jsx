965af2653802ffd52efce3cd6ecfb0525dd249c2
selene-cho <selene.cho29@gmail.com>
2023-04-29 02:00:09 +0900

    fix: 사이드바 스크로롤 수정

--- src/pages/VariablePage/Variable.jsx
+++ src/pages/VariablePage/Variable.jsx
@@ -5,8 +5,8 @@ import styles from './Variable.module.scss';
 
 export default function Home() {
   const [search, setSearch] = useState(''); // 변수 검색 단어
-  const label = '변수명을 입력해주세요.';
-  const placeholder = "'한글 단어' 또는 '영어 + 한글' 단어";
+  const label = '변수명을 입력해주세요. (단어)';
+  const placeholder = "'한글' or '영어+한글'";
 
   return (
     <div className={styles.container}>

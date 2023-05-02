1022eb885ef52bee7aa452cc996c08c88da3ea67
selene-cho <selene.cho29@gmail.com>
2023-05-03 00:41:10 +0900

    api s 추가

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import Cookies from 'js-cookie';
 export async function getTranslateWord(word = '') {
   const query = `word=${word}`;
   const response = await fetch(
-    `http://223.130.128.91/api/v1/variabletranslate/search/?${query}`
+    `https://223.130.128.91/api/v1/variabletranslate/search/?${query}`
   );
   if (!response.ok) {
     throw new Error('변수 추천 데이터를 불러오는데 실패했습니다.');
@@ -19,7 +19,7 @@ export async function getTranslateWord(word = '') {
 export async function getAbbr(word = '') {
   const query = `word=${word}`;
   const response = await fetch(
-    `http://223.130.128.91/api/v1/abbr/search/?${query}`
+    `https://223.130.128.91/api/v1/abbr/search/?${query}`
   );
   if (!response.ok) {
     throw new Error('약어 추천 데이터를 불러오는데 실패했습니다.');
@@ -29,7 +29,7 @@ export async function getAbbr(word = '') {
 }
 
 const instance = axios.create({
-  baseURL: 'http://223.130.128.91/api/v1',
+  baseURL: 'https://223.130.128.91/api/v1',
   headers: { withCredentials: true },
 });
 

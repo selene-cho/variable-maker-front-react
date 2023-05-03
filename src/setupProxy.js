617ae861f0c669b09ff356e30ae39e2a5ae417ed
selene-cho <selene.cho29@gmail.com>
2023-05-03 11:43:51 +0900

    redirect 파일 추가

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/api', {
+      // 백엔드 주소
+      target: 'http://223.130.128.91',
+      changeOrigin: true,
+    })
+  );
+};

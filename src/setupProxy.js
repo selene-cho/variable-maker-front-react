8cd10172d559d60d28b85049c36b85aa5ce1a06a
selene-cho <selene.cho29@gmail.com>
2023-05-03 12:14:27 +0900

    setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,19 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware('/api', {
+//       // 백엔드 주소
+//       target: 'http://223.130.128.91',
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
+const createProxyMiddleware = require('http-proxy-middleware');
+module.exports = (app) => {
   app.use(
     createProxyMiddleware('/api', {
-      // 백엔드 주소
       target: 'http://223.130.128.91',
       changeOrigin: true,
     })

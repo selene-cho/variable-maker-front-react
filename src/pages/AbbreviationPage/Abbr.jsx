a6655d117138c9e6db60c038f62e15cc70769449
yesaroun <akor1@naver.com>
2023-05-04 11:30:53 +0900

    feat: ad 추가

--- src/pages/AbbreviationPage/Abbr.jsx
+++ src/pages/AbbreviationPage/Abbr.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import SearchForm from '../../components/SearchForm';
-import AbbrResult from './AbbrResult';
-import styles from './Abbr.module.scss';
+import { useState } from "react";
+import SearchForm from "../../components/SearchForm";
+import AbbrResult from "./AbbrResult";
+import AbbrAd from "./AbbrAd";
+import styles from "./Abbr.module.scss";
 
 export default function Abbr() {
-  const [abbrSearch, setAbbrSearch] = useState(''); // 약어 추천 변수명 검색수
-  const label = '줄이고 싶은 변수명을 입력해주세요.';
+  const [abbrSearch, setAbbrSearch] = useState(""); // 약어 추천 변수명 검색수
+  const label = "줄이고 싶은 변수명을 입력해주세요.";
   const placeholder = "'영어 or 한글' 변수명";
   return (
     <div className={styles.container}>
@@ -17,6 +18,7 @@ export default function Abbr() {
       >
         abbrSearch
       </SearchForm>
+      <AbbrAd></AbbrAd>
     </div>
   );
 }
--- src/pages/VariablePage/Variable.jsx
+++ src/pages/VariablePage/Variable.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import SearchForm from '../../components/SearchForm';
-import VariableResult from './VariableResult';
-import styles from './Variable.module.scss';
+import { useState } from "react";
+import SearchForm from "../../components/SearchForm";
+import VariableResult from "./VariableResult";
+import VariableAd from "./VariableAd";
+import styles from "./Variable.module.scss";
 
 export default function Home() {
-  const [search, setSearch] = useState(''); // 변수 검색 단어
-  const label = '변수명을 입력해주세요. (단어)';
+  const [search, setSearch] = useState(""); // 변수 검색 단어
+  const label = "변수명을 입력해주세요. (단어)";
   const placeholder = "'한글' or '영어+한글'";
 
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <SearchForm setSearch={setSearch} placeholder={placeholder} label={label}>
         variableSearch
       </SearchForm>
+      <VariableAd />
     </div>
   );
 }
